20c09d54ac5ea4049836ad45642d0667c1af7ac5
FIGUEROA9 <pf.figueroa9@gmail.com>
2025-11-11 21:37:11 -0500

    Add files via upload

---
+++ Dominio.java
@@ -0,0 +1,15 @@
+
+public class Dominio{
+    public static void main(String[] args) {
+        LegalCase caso1 = new LegalCase("Civil", "C-001", "Carlos Pérez");
+        LegalCase caso2 = new LegalCase("Penal", "P-155", "Laura Gómez");
+        LegalCase caso3 = new LegalCase("Laboral", "L-320", "Andrés Torres");
+
+       
+        caso1.showinfo();
+        caso2.showinfo();
+        caso3.showinfo();
+    
+
+    }
+}
---
+++ LegalCase.java
@@ -0,0 +1,19 @@
+class LegalCase {
+    private String typocaso;
+    private String numerocaso;
+    private String abogado;
+
+    public LegalCase(String typocaso, String numerocaso, String abogado) {
+        this.typocaso = typocaso;
+        this.numerocaso = numerocaso;
+        this.abogado = abogado;
+    }
+
+    public void showinfo() {
+        System.out.println("EL TIPO DE CASO QUE SE ESTA EJECUTANDO ES: " + typocaso);
+        System.out.println("CON EL NUMERO DE CASO: " + numerocaso);
+        System.out.println("EL NOMBRE DEL ABOGADO ES: " + abogado);
+        System.out.println("--------------------------------------------");
+    }
+}
+
