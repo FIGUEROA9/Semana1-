ea05a3a6b55e553113d1b9d0dd4bfe2c7930bcb7
FIGUEROA9 <pf.figueroa9@gmail.com>
2025-12-12 11:47:52 -0500

    adiciones mejoras en semana 4

--- src/co/edu/sena/pedrofigueroa/sem4/PenalLawyer.java
+++ src/co/edu/sena/pedrofigueroa/sem4/PenalLawyer.java
@@ -1,6 +1,33 @@
 package src.co.edu.sena.pedrofigueroa.sem4;
+public class PenalLawyer extends Abogado {
 
-public class PenalLawyer {
-    
+    private int casosGanados;
+    private String riesgoCasos;
 
+    public PenalLawyer() {
+        super();
+    }
+
+    public PenalLawyer(String nombre, String email, int numeroCedula, int numeroLicencia, int añosExperiencia,
+                       int casosGanados, String riesgoCasos) {
+        super(nombre, email, numeroCedula, numeroLicencia, añosExperiencia);
+        this.casosGanados = casosGanados;
+        this.riesgoCasos = riesgoCasos;
+    }
+
+    public int getCasosGanados() {
+        return casosGanados;
+    }
+
+    public void setCasosGanados(int casosGanados) {
+        this.casosGanados = casosGanados;
+    }
+
+    public String getRiesgoCasos() {
+        return riesgoCasos;
+    }
+
+    public void setRiesgoCasos(String riesgoCasos) {
+        this.riesgoCasos = riesgoCasos;
+    }
 }
